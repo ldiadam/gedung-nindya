4f17e022c63b78e641ee685906bf4df57eab1880
Rinaldi Adam <rinaldiadam@live.com>
2020-10-15 09:23:09 +0700

    progress

--- app/src/main/java/com/adam/kotlin/Home.kt
+++ app/src/main/java/com/adam/kotlin/Home.kt
@@ -3,6 +3,12 @@ package com.adam.kotlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -11,15 +17,43 @@ class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
+        setSupportActionBar(toolbar)
         auth = FirebaseAuth.getInstance()
 
-        btnLogout.setOnClickListener {
-            auth.signOut()
-            Intent(this@Home, Login::class.java).also {
-                it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                startActivity(it)
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        var appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.nav_home, R.id.nav_profile
+        ).build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        nav_bottom.setupWithNavController(navController)
+
+
+
+        /* btnLogout.setOnClickListener {
+
+        } */
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout -> {
+                auth.signOut()
+                Intent(this@Home, Login::class.java).also {
+                    it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(it)
+                }
+
+                return true
             }
+
+             else -> return true
         }
     }
 }
